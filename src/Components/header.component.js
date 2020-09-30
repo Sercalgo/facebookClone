02724cbb3a340316cabdd5202fa06d04ae0e6218
Sercalgo <Sercalgo@github.com>
2020-10-01 01:02:29 +0200

    Created header of the app with the styles

---
+++ src/Components/header.component.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+import SearchIcon from "@material-ui/icons/Search";
+
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import SupervisedUserCircleRoundedIcon from "@material-ui/icons/SupervisedUserCircleRounded";
+
+import { Avatar, IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+import "./header.styles.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/220px-Facebook_f_logo_%282019%29.svg.png"
+          alt="Facebook Logo"
+        />
+      </div>
+
+      <div className="header__input">
+        <SearchIcon />
+        <input placeholder="Search Facebook" type="text" />
+      </div>
+      <div className="header__center">
+        <div className="header__option header__option--active">
+          <HomeIcon fontsize="large" />
+        </div>
+        <div className="header__option">
+          <FlagIcon fontsize="large" />
+        </div>
+        <div className="header__option">
+          <SubscriptionsOutlinedIcon fontsize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontOutlinedIcon fontsize="large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircleRoundedIcon fontsize="large" />
+        </div>
+
+        <div className="header__right">
+          <div className="header__info">
+            <Avatar />
+            <h4>Sergio</h4>
+            <IconButton>
+              <AddIcon />
+            </IconButton>
+            <IconButton>
+              <ForumIcon />
+            </IconButton>
+            <IconButton>
+              <NotificationsActiveIcon />
+            </IconButton>
+            <IconButton>
+              <ExpandMoreIcon />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
